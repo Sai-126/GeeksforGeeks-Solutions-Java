0dd038fb8a2f47a63aa90f2183cd99eff6e07003
Sai Laxmi Pasagadugula <23a91a4446@aec.edu.in>
2026-05-12 12:41:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Start Coding/start-coding.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        // This is the line that actually outputs the message
+        System.out.println("Hello World");
+    }
+}
