f8cf34802490bdea70589978f7af028d753305d3
Sai Laxmi Pasagadugula <23a91a4446@aec.edu.in>
2026-05-12 12:46:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment/learn-to-comment.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        // Scanner is used to read input values
+        Scanner sc = new Scanner(System.in);
+        
+        // Reading three integers a, b, and c
+        if (sc.hasNextInt()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            // Printing the value of a
+            System.out.println(a);
+            
+            // Commenting out the line that prints the value of b
+            // System.out.println(b);
+            
+            // Printing the value of c
+            System.out.println(c);
+        }
+    }
+}
