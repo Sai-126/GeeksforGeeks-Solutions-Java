204f98545eac46202ab70426b5214c8040c327c6
Sai Laxmi Pasagadugula <23a91a4446@aec.edu.in>
2026-05-12 13:29:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Type Conversion/type-conversion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class GFG {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+
+        int ans = (int)d;
+
+        System.out.println(ans);
+    }
+}
