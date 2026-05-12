20b15a1764f88575766233800fb9da6f18d57033
Sai Laxmi Pasagadugula <23a91a4446@aec.edu.in>
2026-05-12 19:48:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -0,0 +1,32 @@
+class Solution {
+    // Renamed from trappingWater to maxWater to match the driver code
+    public int maxWater(int[] arr) {
+        int n = arr.length;
+        if (n <= 2) return 0;
+
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
+
+        while (left <= right) {
+            // Compare heights at the pointers to decide which side to process
+            if (arr[left] <= arr[right]) {
+                if (arr[left] >= leftMax) {
+                    leftMax = arr[left];
+                } else {
+                    totalWater += leftMax - arr[left];
+                }
+                left++;
+            } else {
+                if (arr[right] >= rightMax) {
+                    rightMax = arr[right];
+                } else {
+                    totalWater += rightMax - arr[right];
+                }
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+}
