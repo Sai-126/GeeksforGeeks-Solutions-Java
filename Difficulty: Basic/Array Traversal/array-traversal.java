0c5a1d2b1828e7b40f15c151c8e3f599c4b2b1ca
Sai Laxmi Pasagadugula <23a91a4446@aec.edu.in>
2026-05-12 12:43:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Traversal/array-traversal.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void arrayTraversal(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // Print the element followed by a space
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
