0ac190346e44debbd5f167424f6d4b0900902427
Sai Laxmi Pasagadugula <23a91a4446@aec.edu.in>
2026-05-12 13:33:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right Angle Triangle/right-angle-triangle.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    void triangleWall(int s) {
+
+        for (int i = 1; i <= s; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
